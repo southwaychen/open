fd653a264724103c102b51e977b4ea0991ac3be2
Tony <southway@qq.com>
2018-07-04 21:12:30 +0800

    fix some bug

--- open-common/src/main/java/com/open/common/entity/po/BasePo.java
+++ open-common/src/main/java/com/open/common/entity/po/BasePo.java
@@ -1,6 +1,5 @@
 package com.open.common.entity.po;
 
-import lombok.Data;
 
 import java.io.Serializable;
 import java.time.ZonedDateTime;
@@ -11,6 +10,19 @@ public class BasePo implements Serializable {
     private Date createdTime = Date.from(ZonedDateTime.now().toInstant());
     private Date updatedTime = Date.from(ZonedDateTime.now().toInstant());
 
+    public Date getCreatedTime() {
+        return createdTime;
+    }
 
+    public void setCreatedTime(Date createdTime) {
+        this.createdTime = createdTime;
+    }
 
+    public Date getUpdatedTime() {
+        return updatedTime;
+    }
+
+    public void setUpdatedTime(Date updatedTime) {
+        this.updatedTime = updatedTime;
+    }
 }
