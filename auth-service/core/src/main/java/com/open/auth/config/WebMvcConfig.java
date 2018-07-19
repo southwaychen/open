b4013d6276af7df5bc495e3130123ba892b25651
陈楠炜 <chennanwei@crc.com.hk>
2018-07-19 16:49:00 +0800

    init

--- auth-service/core/src/main/java/com/open/auth/config/WebMvcConfig.java
+++ auth-service/core/src/main/java/com/open/auth/config/WebMvcConfig.java
@@ -5,7 +5,7 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
